3885fee8340a020d92ae444dadb7e9cb3572e251
aidarkirpich <aidarooooo@yandex.ru>
2023-12-25 17:50:52 +0500

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+@Component("timer")
+public class Timer {
+
+    private static final Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
